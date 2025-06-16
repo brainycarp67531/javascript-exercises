a3aa62dbfd6d874744c49436e25cac4438e12edb
Björn Sterling <bjorn.sterling@gmail.com>
2025-06-16 18:36:27 +0200

    completed number 08 after some strugle

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,15 +1,29 @@
 const removeFromArray = function(array, ...args) {
 
+    // Create new empty array called newArray. 
+    let newArray = [];
+
+    // use forEach to go through the array 
+    array.forEach((item) => {
+        // use push the push element to the new array 
+        // unless it is included in the function arguments
+        if (!args.includes(item)) {
+            newArray.push(item);
+        }
+    });    
+    
+    
+    
+    
     // Loop over all the elements in args
-    for (item in args) {
+    // for (const item of args) {
 
-        // for each element check if it in the array and remove it. 
-        array = array.filter(arrayItem => arrayItem !== item);
-
-    }
+    //     // for each element check if it in the array and remove it. 
+    //     array = array.filter(arrayItem => arrayItem !== item);
+    // };
 
     // return the modified array
-    return array;
+    return newArray;
 };
 
 // Do not edit below this line
