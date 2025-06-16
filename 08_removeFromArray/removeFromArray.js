1e2816bbf480aba5252f6153c97367768f7bb4e0
brainycarp67531 <108860913+brainycarp67531@users.noreply.github.com>
2025-06-16 10:13:01 +0200

    Started with number 8
    Created and function to filter an array using .filter

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+
+    // Loop over all the elements in args
+    for (item in args) {
+
+        // for each element check if it in the array and remove it. 
+        array = array.filter(arrayItem => arrayItem !== item);
+
+    }
+
+    // return the modified array
+    return array;
 };
 
 // Do not edit below this line
