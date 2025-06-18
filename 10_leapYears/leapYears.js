595a1fa83cade3c354e90904a6cf2690d9768212
brainycarp67531 <108860913+brainycarp67531@users.noreply.github.com>
2025-06-18 06:18:18 +0200

    10 and 11
    completed task 10 and 11, need to test them before moving to the next one.

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,24 @@
-const leapYears = function() {
+const leapYears = function(year) {
+ 
 
+    // check if year is divisible by 400 which makes it a leap year.
+    if (year % 400 === 0) {
+        
+        // return that is a leap year
+        return true;
+    }
+
+    // Check it year is divisible by 4 and not 100, which makes it a leap year. 
+    else if (year % 4 === 0 && year % 100 !== 0) {
+        // return that it is a leap year
+        return true;
+    }
+
+    // else it is not a leap year. 
+    else {
+        // return that it is not a leap year. 
+        return false;
+    }
 };
 
 // Do not edit below this line
--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,26 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFahrenheit) {
+
+  // convert the temperature from fahrenheit to celsius
+  let tempCelsius = (tempFahrenheit - 32) * 5 / 9;
+
+  // round the value to 1 decimal
+  tempCelsius = Math.round(tempCelsius * 10) / 10;
+
+  // return the calculated value.
+  return tempCelsius;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCelsius) {
+
+  // convert the temperature from celsius to Fahrenheit
+  let tempFahrenheit = tempCelsius * 9 / 5 + 32;
+
+  // round the value to 1 decimal
+  tempFahrenheit = Math.round(tempFahrenheit * 10) / 10;
+
+  // return the calculated value
+  return tempFahrenheit;
 };
 
 // Do not edit below this line
