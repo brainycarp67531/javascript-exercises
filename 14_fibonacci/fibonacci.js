2dc13005dab6f8b6fdafd54963c9f02fc0c6c213
brainycarp67531 <108860913+brainycarp67531@users.noreply.github.com>
2025-08-14 13:08:34 +0200

    Refactor fibonacci to return nth value and handle errors
    Updated the fibonacci function to return the nth Fibonacci number instead of an array, handle negative inputs by returning 'OOPS', and accept numeric strings as input. Also enabled the test for negative input handling.

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,12 +1,26 @@
 const fibonacci = function(n) {
 
-  const fib = [1, 1];
-  for (let i = 2; i < n; i++) {
-    fib[i] = fib[i - 1] + fib[i - 2];
-  };
-  return fib.slice(0, n);
+    // check if it is a negative number and give back the error message "OOPS"
+    if (n < 0) {
+        return "OOPS";
+    };
+
+    // check if it is a string and still a number and then convert it to a number. 
+    if (n >= "0" || n <= "9") {
+        // convert it to a number
+        n = parseInt(n);
+    };
+
+    // calculate the n:th fibonacci number
+    let a = 0, b = 1;
+    for (let i = 0; i < n; i++) {
+        [a, b] = [b, a + b];
+    };
+    
+    // return the n:th fibonacci number
+    return a;
 };
 
-
+ 
 // Do not edit below this line
 module.exports = fibonacci;
