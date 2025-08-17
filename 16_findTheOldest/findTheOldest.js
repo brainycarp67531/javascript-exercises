e229cc391cd6d16f71a878abfefbd9da6a60776c
Björn Sterling <bjorn.sterling@gmail.com>
2025-08-17 19:57:59 +0200

    started with number 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,4 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+
+    // use the reduce method to calculate the age of the person. 
+    let oldest = people.reduce((previousPerson, currentPerson) => {
+        // test to see if yearOfDeath is missing and if so use current year to calculate the age
+        // need to test both for previous and for current. 
+
+        return (previousPerson.yearOfDeath - previousPerson.yearOfBirth) >= (currentPerson.yearOfDeath - currentPerson.yearOfBirth) ? previousPerson : currentPerson;
+    });
+
+    // return the oldest person in the array. 
+    return oldest
 
 };
 
