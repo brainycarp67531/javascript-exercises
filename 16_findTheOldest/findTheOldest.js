1f74560421d41d9c42963dbbd6e1aff38569cbe5
brainycarp67531 <108860913+brainycarp67531@users.noreply.github.com>
2025-08-18 13:45:13 +0200

    Handle missing yearOfDeath in findTheOldest
    Updated findTheOldest to correctly handle people without a yearOfDeath by using the current year for age calculation. This ensures the function works for living people as well as those with a yearOfDeath.

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,15 +1,27 @@
 const findTheOldest = function(people) {
 
+    // store the current year in a variable named currentYear. 
+    const currentYear = new Date().getFullYear();
+
     // use the reduce method to calculate the age of the person. 
     let oldest = people.reduce((previousPerson, currentPerson) => {
         // test to see if yearOfDeath is missing and if so use current year to calculate the age
-        // need to test both for previous and for current. 
+        // need to test both for previous and for current.
+        // if yearOfDeath is missing in the previousPerson then use the current year 
+        if (!("yearOfDeath" in previousPerson)) {
+            return (currentYear - previousPerson.yearOfBirth) >= (currentPerson.yearOfDeath - currentPerson.yearOfBirth) ? previousPerson : currentPerson;
+        // if yearOfDeath is missing in the currentPerson then use the current year. 
+        } else if (!("yearOfDeath" in currentPerson)) {
+            return (previousPerson.yearOfDeath - previousPerson.yearOfBirth) >= (currentYear - currentPerson.yearOfBirth) ? previousPerson : currentPerson;
+        } else {
+            // if yearOdDeath is missing in the currentPerson then use the current year
+            return (previousPerson.yearOfDeath - previousPerson.yearOfBirth) >= (currentPerson.yearOfDeath - currentPerson.yearOfBirth) ? previousPerson : currentPerson;
+        };
 
-        return (previousPerson.yearOfDeath - previousPerson.yearOfBirth) >= (currentPerson.yearOfDeath - currentPerson.yearOfBirth) ? previousPerson : currentPerson;
     });
 
     // return the oldest person in the array. 
-    return oldest
+    return oldest;
 
 };
 
